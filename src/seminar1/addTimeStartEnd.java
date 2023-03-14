51855018623b9a674103ed078e7bfe641974cd4b
Vadik <gloom16@mail.ru>
2023-03-15 02:12:29 +0300

    Применение таймера и сравнение работы ArrayList и LinkedList

---
+++ src/seminar1/addTimeStartEnd.java
@@ -0,0 +1,27 @@
+package seminar1;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class addTimeStartEnd {
+    public static void main(String[] args) {
+        List<Integer> arrayList = new ArrayList<>();
+        Random random = new Random();
+        long startTimeArray = System.nanoTime();
+        for (int i = 0; i < 1_000_000; i++) {
+            arrayList.add(5);
+        }
+        long endTimeArray = System.nanoTime();
+        List<Integer> linkedList = new LinkedList<>();
+        long startTimeLinked = System.nanoTime();
+        for (int i = 0; i < 1_000_000; i++) {
+            linkedList.add(5);
+        }
+        long endTimeLinked = System.nanoTime();
+        System.out.println("Время заполнения ArrayList = " + (endTimeArray - startTimeArray));
+        System.out.println("Время заполнения LinkedList = " + (endTimeLinked - startTimeLinked));
+        System.out.println("Разница между ArrayList и LinkedList = " + ((endTimeArray - startTimeArray) - (endTimeLinked - startTimeLinked)));
+    }
+}
